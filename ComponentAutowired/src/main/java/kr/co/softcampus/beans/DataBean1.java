ec4cbdeb28471804926a9ddaade8d1e0fbd9cac0
seong <59963039+seong7517@users.noreply.github.com>
2021-10-08 09:28:07 +0900

    컴포넌트 자동주입
    Component Autowired

---
+++ ComponentAutowired/src/main/java/kr/co/softcampus/beans/DataBean1.java
@@ -0,0 +1,12 @@
+package kr.co.softcampus.beans;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class DataBean1 {
+	
+	public DataBean1() {
+//		System.out.println("DataBean1의 생성자");
+	}
+	
+}
